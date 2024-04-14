3fdd2d56e23706a52bed1f2b3067ed5b6f65e144
Nils van Lück <nils@vanlueck.dev>
2024-04-14 18:59:01 +0200

    feat: add server model

---
+++ GsmCore/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GsmCore.Models;
+
+[PrimaryKey(nameof(Id))]
+public abstract class BaseModel
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ GsmCore/Models/Server.cs
@@ -0,0 +1,11 @@
+﻿namespace GsmCore.Models;
+
+public class Server : BaseModel
+{
+    public string Name { get; set; }
+    public string BindIp { get; set; }
+    public uint GamePort { get; set; }
+    public uint QueryPort { get; set; }
+    public string Map { get; set; }
+    public uint Slots { get; set; }
+}
