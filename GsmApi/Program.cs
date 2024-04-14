01267ceda759f6e4f93596168c84d4b5225a65e7
C#-Aggu <tobias.schoeler@outlook.de>
2024-04-14 21:05:18 +0200

    feat: windows service

--- GsmApi/Program.cs
+++ GsmApi/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddWindowsService();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
