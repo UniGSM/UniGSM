84bf7d16262d3bb3210dc76d5723e611ed6c1fb7
Nils van Lück <nils@vanlueck.dev>
2024-04-14 19:05:29 +0200

    feat: add fields to server

--- GsmCore/Models/Server.cs
+++ GsmCore/Models/Server.cs
@@ -1,11 +1,23 @@
-﻿namespace GsmCore.Models;
+﻿using System.ComponentModel;
+
+namespace GsmCore.Models;
 
 public class Server : BaseModel
 {
     public string Name { get; set; }
-    public string BindIp { get; set; }
+    [DefaultValue("0.0.0.0")] public string BindIp { get; set; }
     public uint GamePort { get; set; }
     public uint QueryPort { get; set; }
+
+    [DefaultValue("dayzOffline.chernarusplus")]
     public string Map { get; set; }
-    public uint Slots { get; set; }
+
+    [DefaultValue(32)] public uint Slots { get; set; }
+    [DefaultValue(true)] public bool AutoStart { get; set; }
+    [DefaultValue(true)] public bool AutoRestart { get; set; }
+    [DefaultValue(true)] public bool AutoUpdate { get; set; }
+    [DefaultValue(true)] public bool DoLogs { get; set; }
+    [DefaultValue(true)] public bool AdminLog { get; set; }
+    [DefaultValue(true)] public bool NetLog { get; set; }
+    public string AdditionalStartParams { get; set; }
 }
