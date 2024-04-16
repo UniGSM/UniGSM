ac71faa47d26afc2f572d5fa28a8af0a4eafe6eb
Nils van Lück <nils@vanlueck.dev>
2024-04-16 18:19:22 +0200

    feat: add wip help text

--- GsmCLI/Program.cs
+++ GsmCLI/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+//write help text
+Console.WriteLine("Usage: dayzgsmcli [options] [command]");
+Console.WriteLine("Options:");
+Console.WriteLine("  -h, --help    Show help text");
+Console.WriteLine("  -v, --version Show version information");
+
+Console.WriteLine("Commands:");
+Console.WriteLine("  server         Server commands");
+Console.WriteLine("  backup         Backup commands");
+Console.WriteLine("  settings       Settings commands");
