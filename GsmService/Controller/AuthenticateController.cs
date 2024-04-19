9ca793a5c1fee48aa6af5e66aa9eafd3bcb1bcd1
Nils van Lück <nils@vanlueck.dev>
2024-04-19 17:44:09 +0200

    feat: add token refresh

--- GsmService/Controller/AuthenticateController.cs
+++ GsmService/Controller/AuthenticateController.cs
@@ -2,9 +2,8 @@
 using System.Security.Claims;
 using System.Text;
 using GsmApi.Authentication;
-using GsmApi.DTO;
 using GsmCore.ApiClient;
-using GsmCore.Util;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -25,6 +24,40 @@ public class AuthenticateController(
     {
         var user = await userManager.FindByNameAsync(model.UserName);
         if (user == null || !await userManager.CheckPasswordAsync(user, model.Password)) return Unauthorized();
+        var userRoles = await userManager.GetRolesAsync(user);
+
+        var authClaims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.UserName),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+        authClaims.AddRange(userRoles.Select(userRole => new Claim(ClaimTypes.Role, userRole)));
+
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["JWT:ValidIssuer"],
+            audience: configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddHours(3),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token),
+            expiration = token.ValidTo
+        });
+    }
+
+    [HttpPost]
+    [Route("token/refresh")]
+    [Authorize]
+    public async Task<IActionResult> RefreshToken()
+    {
+        var userName = User.Identity?.Name;
+        var user = await userManager.FindByNameAsync(userName);
+        if (user == null) return Unauthorized();
         var userRoles = await userManager.GetRolesAsync(user);
 
         var authClaims = new List<Claim>
