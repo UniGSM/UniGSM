30017097a1f8aa5642eef3d8dbabfc88d48e0655
Nils van Lück <nils@vanlueck.dev>
2024-04-22 23:20:09 +0200

    feat: add webview2

---
+++ GsmWebView/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace GsmWebView;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--no-proxy-server");
+        InitializeComponent();
+    }
+}
